6dd825a4cc2994ff951d8e4df8fb9bbca6aaf8a4
Chie <akiba.chie@gmail.com>
2016-09-08 16:44:28 -0700

    Initial commit

---
+++ actions.js
@@ -0,0 +1,6 @@
+//Need to be able to do the following in the game
+  //Guess a number
+  //Get feedback as to how close or far you are from the right number
+  //Show the number you just guessed
+  //Have the input entry clear your entry
+  //If guessed the right number allow option to play again
